2a1b060ffd5bdee5d1454d6c2005c44558adeb13
Phan Văn Thiện <94745695+Thienvp@users.noreply.github.com>
2023-07-11 19:35:45 +0700

    Frontend_Demo

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('demoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Lấy giá trị từ trường văn bản
+    var inputText = document.getElementById('textInput').value;
+  
+    // Gửi yêu cầu HTTP POST đến API /predict
+    axios.post('127.0.0.1/predict', {
+        text: inputText
+      })
+      .then(function (response) {
+        // Hiển thị kết quả từ API
+        var resultDiv = document.getElementById('result');
+        var sentimentText = document.getElementById('sentimentText');
+        sentimentText.innerText = 'Message: ' + response.data.message + ', Score: ' + response.data.score;
+        resultDiv.classList.remove('hidden');
+      })
+      .catch(function (error) {
+        // Xử lý lỗi nếu có
+        console.error('Lỗi:', error);
+      });
+  });
+  
